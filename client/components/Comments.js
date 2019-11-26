9b4c3b6695a924b8f201e2ea52f8ef0c2ddd9673
Eddie Hsu <aoe102198@gmail.com>
2019-11-26 14:18:35 +0800

    addComment function implement and reset the form when submit

--- client/components/Comments.js
+++ client/components/Comments.js
@@ -18,7 +18,8 @@ const Comments = React.createClass({
         const { postId } = this.props.params;
         const author = this.refs.author.value;
         const comment = this.refs.comment.value;
-        this.props.addComment(postId,author,comment);
+        this.props.addComment(postId, author, comment);
+        this.refs.commentForm.reset();
     },
 
 	render() {
--- client/reducers/comments.js
+++ client/reducers/comments.js
@@ -1,6 +1,32 @@
+// Reducer Composition structure.
+function postComments(state = [], action){
+    switch(action.type) {
+        case "ADD_COMMENT" :
+            // return the new state with the new comment.
+            return [ ...state,{
+                user: action.author,
+                text: action.comment
+            }]
+        case "REMOVE_COMMENT" :
+
+        default:
+            return state;
+    }
+    return state;
+}
+
+
 // initial the state with empty array.
 function comments(state = [], action){
     // console.log(state, action);
+    if(typeof action.postId !== "undefined"){
+        return {
+            // copy the current state
+            ...state,
+            // overwrite this post with a new one
+            [action.postId]: postComments(state[action.postId], action)
+        }
+    }
     return state;
 }
 
